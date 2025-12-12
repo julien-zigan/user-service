45176dcfea4dd5e695921cb549e6c4e8e565d9f0
Julien Zigan <zigan.julien@gmail.com>
2025-12-12 15:50:58 +0100

    db: add h2, jpa-auto-ddl and minimal user entity

--- src/main/java/de/steinuntersteinen/userservice/infra/persistence/UserEntity.java
+++ src/main/java/de/steinuntersteinen/userservice/infra/persistence/UserEntity.java
@@ -1,4 +1,23 @@
 package de.steinuntersteinen.userservice.infra.persistence;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Table(
+        name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "uK_users_email", columnNames = "email")
+        }
+)
 public class UserEntity {
+    @Id
+    @UuidGenerator
+    @Column(name = "id", updatable = false)
+    UUID id;
+
+    @Column(name = "email", nullable = false)
+    String email;
 }
