51b6188b051b7473fbaf3821fc327e92d1896294
Julien Zigan <zigan.julien@gmail.com>
2025-12-13 15:39:00 +0100

    test: separate user creation and event test

--- src/test/java/de/steinuntersteinen/userservice/application/services/UserServiceImplTest.java
+++ src/test/java/de/steinuntersteinen/userservice/application/services/UserServiceImplTest.java
@@ -1,14 +1,15 @@
 package de.steinuntersteinen.userservice.application.services;
 
 import de.steinuntersteinen.userservice.domain.aggregates.User;
+import de.steinuntersteinen.userservice.domain.events.DomainEvent;
+import de.steinuntersteinen.userservice.domain.events.UserCreated;
 import de.steinuntersteinen.userservice.domain.valueobjects.UserId;
-import de.steinuntersteinen.userservice.domain.valueobjects.Email;
 import de.steinuntersteinen.userservice.domain.repositories.UserRepository;
 import de.steinuntersteinen.userservice.infra.events.DomainEventPublisher;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 
-import java.util.UUID;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
@@ -16,7 +17,7 @@ import static org.mockito.Mockito.*;
 class UserServiceTest {
 
     @Test
-    void registerUserShouldCreateUserAndPublishEvents() {
+    void registerUserShouldCreateUser() {
         UserRepository repository = mock(UserRepository.class);
         DomainEventPublisher eventPublisher = mock(DomainEventPublisher.class);
         UserService service = new UserServiceImpl(eventPublisher, repository);
@@ -30,7 +31,26 @@ class UserServiceTest {
         User savedUser = userCaptor.getValue();
         assertThat(savedUser.getUserId()).isEqualTo(userId);
         assertThat(savedUser.getEmail().value()).isEqualTo(email);
+    }
 
-        verify(eventPublisher).publish(savedUser.pullDomainEvents());
+    @Test
+    void registerUser_ShouldPublishEvent() {
+        UserRepository repository = mock(UserRepository.class);
+        DomainEventPublisher eventPublisher = mock(DomainEventPublisher.class);
+        UserService service = new UserServiceImpl(eventPublisher, repository);
+        ArgumentCaptor<List<DomainEvent>> captor = ArgumentCaptor.forClass(List.class);
+        String testEmail = "test@email.com";
+
+        service.registerUser(testEmail);
+
+        verify(eventPublisher).publish(captor.capture());
+
+        List<DomainEvent> events = captor.getValue();
+
+        assertThat(events).hasSize(1);
+        assertThat(events.get(0)).isInstanceOf(UserCreated.class);
+
+        UserCreated event = (UserCreated) events.get(0);
+        assertThat(event.email().value()).isEqualTo(testEmail);
     }
 }
